f17d7c73123900ea04cdb460e59b4f7ad5ff0caa
andrewrennie <andrew.rennie@me.com>
2015-02-04 20:39:41 +0000

    Add BCrypt and fix original db

--- db/migrate/20150109120028_todos_updatedeadlines.rb
+++
@@ -1,6 +0,0 @@
-class TodosUpdatedeadlines < ActiveRecord::Migration
-  def change
-add_column :todos, :todo_deadlinetime, :time
-change_column :todos, :todo_deadline, :date
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,28 +11,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150109124002) do
+ActiveRecord::Schema.define(version: 20150204203124) do
 
-  create_table "todos", force: :cascade do |t|
-    t.text     "todo_item",          limit: 65535
-    t.string   "todo_for",           limit: 255
+  create_table "todos", force: true do |t|
+    t.text     "todo_item"
+    t.string   "todo_for"
     t.date     "todo_deadline"
-    t.integer  "todo_importance",    limit: 4
-    t.decimal  "todo_urgence",                     precision: 10, scale: 4
-    t.decimal  "todo_timerequired",                precision: 10, scale: 4
-    t.boolean  "todo_recurring",     limit: 1
-    t.string   "todo_frequency",     limit: 255
-    t.decimal  "todo_status",                      precision: 10, scale: 4
-    t.decimal  "todo_timeremaining",               precision: 10, scale: 4
-    t.decimal  "todo_ranking",                     precision: 10, scale: 4
-    t.boolean  "todo_complete",      limit: 1
-    t.datetime "created_at",                                                null: false
-    t.datetime "updated_at",                                                null: false
-    t.string   "todo_category",      limit: 255
-    t.string   "todo_project",       limit: 255
-    t.string   "todo_user",          limit: 255
+    t.integer  "todo_importance"
+    t.decimal  "todo_urgence",       precision: 10, scale: 4
+    t.decimal  "todo_timerequired",  precision: 10, scale: 4
+    t.boolean  "todo_recurring"
+    t.string   "todo_frequency"
+    t.decimal  "todo_status",        precision: 10, scale: 4
+    t.decimal  "todo_timeremaining", precision: 10, scale: 4
+    t.decimal  "todo_ranking",       precision: 10, scale: 4
+    t.boolean  "todo_complete"
+    t.datetime "created_at",                                  null: false
+    t.datetime "updated_at",                                  null: false
+    t.string   "todo_category"
+    t.string   "todo_project"
+    t.string   "todo_user"
     t.date     "todo_enddate"
     t.time     "todo_deadlinetime"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "password_hash"
+    t.string   "password_salt"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
