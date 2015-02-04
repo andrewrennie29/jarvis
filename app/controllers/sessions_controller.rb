c3c4bba00df8634954db16b01500023c97093baf
andrewrennie <andrew.rennie@me.com>
2015-02-04 22:27:09 +0000

    Finished session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,33 @@ end
 
 def create
 
+@user = User.authenticate(params[:username], params[:password])
+
+if @user
+
+	flash[:notice] = "Login Successful!"
+
+	session[:user_id] = @user.id
+
+	redirect_to 'todos/index'
+
+else
+
+	flash[:alert] = 'You fail at logging in'
+
+	redirect_to login_path
+
+end
+
 end
 
 def destroy
+
+session[:user_id] = nil
+
+flash[:notice] = 'Logged out already? Guess you must have finished your work!'
+
+redirect_to '/'
 
 end
 
