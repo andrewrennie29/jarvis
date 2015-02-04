9c2516de72012c5cf95bf5b6cd8bc76e1c8da43b
andrewrennie <andrew.rennie@me.com>
2015-02-04 20:54:38 +0000

    Add New User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,21 @@ end
 
 def create
 
+	@user = User.new(user_params)
+	if @user.save
+		flash[:notice] = "Sign up successful"
+		redirect_to 'todos/index'
+	else
+		flash[:alert] = "#fail! Please try again."
+		redirect_to :back
+	end
+	
+end
+
+def user_params
+
+	params.require(:user).permit(:username, :email, :password, :confirm_password)
+
 end
 
 end
