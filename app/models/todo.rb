998cef5f3fad2c46b36bc7647e6d0ce452d209d9
andrewrennie <andrew.rennie@me.com>
2015-02-06 07:06:11 +0000

    Error checking if todo does not exist

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -5,15 +5,24 @@ class Todo < ActiveRecord::Base
 def self.allowview(todo_id, session_user_id)
 
   t=Todo.find_by_id(todo_id)
-
-  if t.user_id == session_user_id
-
-    todo_id
-
-  else
+  c=Todo.where('id = ?', todo_id).count
+  
+  if c == 0
 
     nil
 
+  else
+
+    if t.user_id != session_user_id || 
+
+      nil
+
+    else
+
+      todo_id
+
+    end
+  
   end
 
 end
