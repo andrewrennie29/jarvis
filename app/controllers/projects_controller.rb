80d9325baf3d219b6d5644f1cc53df4433e5aa5d
andrewrennie <andrew.rennie@me.com>
2015-02-08 17:06:37 +0000

    fix projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -8,7 +8,7 @@ class ProjectsController < ApplicationController
 
     else
 
-      @projects = Todo.select("todo_project, max(todo_deadline) as project_deadline, sum(if(todo_complete is null,1,0)) as todo_item_count, sum(if(todo_complete=true,0,todo_timeremaining)) as time_remaining, 100-round(sum(if(todo_complete=true,0,todo_timeremaining))*100/sum(todo_timerequired),0) as percentage_complete").where("todo_project is not null and todo_project !='' AND sum(if(todo_complete = true,0,1))>0	 AND user_id = ?", session[:user_id]).group("todo_project")
+      @projects = Todo.select("todo_project, max(todo_deadline) as project_deadline, sum(if(todo_complete is null,1,0)) as todo_item_count, sum(if(todo_complete=true,0,todo_timeremaining)) as time_remaining, 100-round(sum(if(todo_complete=true,0,todo_timeremaining))*100/sum(todo_timerequired),0) as percentage_complete").where("todo_project is not null and todo_project !='' AND user_id = ?", session[:user_id]).group("todo_project")
       @new_todo = Todo.new
       render :index
 
